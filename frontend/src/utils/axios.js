0c5d91d0bdeea5f1a96b29b8981806192e0e1935
shivamani74 <shivamaniboina@gmail.com>
2026-03-10 23:55:16 +0530

    Update axios baseURL to remove '/api' suffix

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://griethub.onrender.com/api" || "/api",
+  baseURL: "https://griethub.onrender.com",
 });
 
 instance.interceptors.request.use((config) => {
